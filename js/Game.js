bbca40c4fde017abfbda6eaba8548c587fdf5454
Tanmay <anjudeviaj2009@gmail.com>
2021-01-18 14:47:52 +0530

    modified game

--- js/Game.js
+++ js/Game.js
@@ -23,4 +23,28 @@ class Game {
       form.display();
     }
   }
+  play(){
+    form.hide();
+    textSize(30);
+    text("Game Start",120,100);
+    Player.getPlayerInfo();
+    if(allPlayers!==undefined){
+      var display_position=130;
+      for(var plr in allPlayers){
+        if(plr==="player"+player.index)
+          fill("red");
+          else{
+            fill("black");
+          }
+          display_position+=20;
+          textSize(15);
+          text(allPlayers[plr].name+": "+allPlayers[plr].distance,120,display_position);
+
+      }
+    }
+    if(keyDown(UP_ARROW)&&player.index!==null){
+      player.distance+=50;
+      player.update();
+    }
+  }
 }
